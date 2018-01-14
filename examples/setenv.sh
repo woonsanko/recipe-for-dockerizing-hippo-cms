16378a9aa333dee12a6623b8691b54e6f384f25a
Woonsan Ko <woonsanko@users.noreply.github.com>
2018-01-13 22:16:17 -0500

    Update setenv.sh

--- examples/setenv.sh
+++ examples/setenv.sh
@@ -2,9 +2,9 @@
 # ${CATALINA_BASE}/bin/setenv.sh
 #
 # Note: as most environment settings are done in Dockerfile, this is almost empty,
-#       except of executing the optional index environment setting script below.
+#       except of executing the optional index initializing script below.
 #
 
-if [ -r "$CATALINA_BASE/bin/indexenv.sh" ]; then
-  . "$CATALINA_BASE/bin/indexenv.sh"
+if [ -r "$CATALINA_BASE/bin/index-init.sh" ]; then
+  . "$CATALINA_BASE/bin/index-init.sh"
 fi
