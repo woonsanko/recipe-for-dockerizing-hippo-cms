966180e82833313886197b6a3741188c3cf61c12
Woonsan Ko <woonsanko@users.noreply.github.com>
2018-01-13 19:15:57 -0500

    Create indexenv.sh

---
+++ examples/indexenv.sh
@@ -0,0 +1,19 @@
+#
+# ${CATALINA_BASE}/bin/indexenv.sh
+#
+# Note: this script assumes that the latest index export zip file could be shared at
+#       ${INDEX_EXPORT_ZIP} (configured in Dockerfile) through a volume.
+#
+# Ref) https://www.onehippo.org/library/enterprise/enterprise-features/lucene-index-export/lucene-index-export.html
+#
+
+echo "Checking if index directory exists at $REPO_PATH/workspaces/default/index..."
+
+if [ ! -d "$REPO_PATH/workspaces/default/index" ]; then
+  if [ -f "${INDEX_EXPORT_ZIP}" ]; then
+    echo "Creating index directory at $REPO_PATH/workspaces/default/index..."
+    mkdir -p $REPO_PATH/workspaces/default/index/
+    echo "Unzipping index export zip, ${INDEX_EXPORT_ZIP}, to $REPO_PATH/workspaces/default/index/..."
+    unzip ${INDEX_EXPORT_ZIP} -d $REPO_PATH/workspaces/default/index/
+  fi
+fi
